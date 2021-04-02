23d55d95d43f7c2945094e3b91a18f85f6deaf2b
Bird Egop <sampletext32@bk.ru>
2021-04-02 17:53:02 +0300

    Add project

---
+++ Mironova_1/Mironova_1/Mironova_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(0, "Russian");
+	double a, b, alpha;
+	cout << "a(большее основание):" << endl;
+	cin >> a;
+	cout << "b(меньшее основание):" << endl;
+	cin >> b;
+	cout << "alpha:" << endl;
+	cin >> alpha;
+
+	double h = (a - b)*tan(alpha)/2; //нахожу высоту через тангенс
+
+	double square = (a + b) * h / 2;//нахожу площадь
+	cout << square;
+}
+
